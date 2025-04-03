52b7252e46f7987c6b172c7a10f6841f19b08903
210130107079 <122084645+210130107079@users.noreply.github.com>
2025-04-03 18:22:55 +0530

    Update cloudinary.js

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config()
 
 cloudinary.config({
-  cloud_name:"dojgeecbw",
-  api_key: 492774556351989,
-  api_secret:"iSZlmQt3-DCz8Nq5biWT4ljAIZQ",
+  cloud_name:"",
+  api_key: ,
+  api_secret:"",
 });
 
 export default cloudinary;
